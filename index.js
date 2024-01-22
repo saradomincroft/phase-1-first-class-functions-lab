c32a77902dd98fd00739ef870af8d8a0805c5944
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-01-22 22:20:34 +1100

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => ['Antonia', 'Nuru', 'Amari', 'Mo'].slice(0, 2);
+
+const returnLastTwoDrivers = (drivers) => ['Antonia', 'Nuru', 'Amari', 'Mo'].slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => (fare) => fare * multiplier;
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (arrayOfDrivers, selectFunction) => {
+    return selectFunction(arrayOfDrivers);
+  };
